03438e63a2824f12f5d0cf3b155726cca0b561ee
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2022-03-02 02:02:01 +0100

    Completes the app js logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './style.css';
-// import Icon from './icon.png';
+import boardScores from './modules/boardScores.js';
+
+const showScores = document.getElementById('scores');
+
+const displayBoard = () => {
+  boardScores.forEach((myScore) => {
+    const scoreList = document.createElement('li');
+    scoreList.innerHTML = `${myScore.name}: ${myScore.score}`;
+    showScores.appendChild(scoreList);
+  });
+};
+displayBoard();
